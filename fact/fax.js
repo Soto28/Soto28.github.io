defa4baf58d9ff2bd213e04b85ce6278b29f45d2
Soto28 <77351285+Soto28@users.noreply.github.com>
2021-01-21 11:18:17 -0800

    Add files via upload

---
+++ fact/fax.js
@@ -0,0 +1,15 @@
+function fact1(){
+	document.getElementById("fact").innerHTML = "No number from 1 to 999 includes the letter “a” in its word form.";
+}
+function fact2(){
+	document.getElementById("fact").innerHTML = "All odd numbers have the letter “e” in its word form.";
+}
+function fact3(){
+	document.getElementById("fact").innerHTML = "The electric chair was invented by a dentist.";
+}
+function fact4(){
+	document.getElementById("fact").innerHTML = "More people are killed annually by donkeys than airplane crashes.";
+}
+function fact5(){
+	document.getElementById("fact").innerHTML = "Los Angeles's full name is: “El Pueblo de Nuestra Senora la Reina de losAngeles de Poriuncula” and can be abbreviated to 3.63% of its size, “LA.” ";
+}
